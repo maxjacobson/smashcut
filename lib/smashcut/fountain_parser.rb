84e3efeb6831cd225009b7a11a1ce6827b20d68a
Max Jacobson <max@hardscrabble.net>
2014-04-19 18:09:35 -0400

    Begin work on parser -- first tiny spec passes

--- lib/smashcut/fountain_parser.rb
+++ lib/smashcut/fountain_parser.rb
@@ -2,6 +2,17 @@ require "parslet"
 
 class Smashcut
   class FountainParser < Parslet::Parser
+    rule(:line_break) { match("\n") }
+    rule(:scene_heading) { match("[A-Z \-\. ]").repeat(1) >> line_break.maybe }
+    root(:scene_heading)
+
+    #def parse(text)
+    #  begin
+    #    super(text)
+    #  rescue Exception => e
+    #    puts e
+    #  end
+    #end
   end
 end
 
