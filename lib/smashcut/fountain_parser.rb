5860d61b21abaa15e8bbb2fc7d1834e89a8ba484
Max Jacobson <max@hardscrabble.net>
2014-04-22 08:24:07 -0400

    rearrange parslet rules -- parsing is hard

--- lib/smashcut/fountain_parser.rb
+++ lib/smashcut/fountain_parser.rb
@@ -4,9 +4,11 @@ class Smashcut
   class FountainParser < Parslet::Parser
     rule(:line_break) { match("\n") }
     rule(:scene_heading) { match("[A-Z \-\. ]").repeat(1) >> line_break.maybe >> line_break.maybe }
+    rule(:character_name) { match("[A-Z ]").repeat(1) >> line_break }
+    rule(:dialogue_speech) { match("[a-zA-Z\s\.\(\)\,0-9\?]").repeat(1) >> line_break.maybe >> line_break.maybe }
+    rule(:dialogue_block) { character_name >> line_break >> action >> line_break.maybe >> line_break.maybe }
     rule(:action) { match("[a-zA-Z\s\.\(\)\,0-9]").repeat(1) >> line_break.maybe >> line_break.maybe }
-    rule(:dialogue) { match("[A-Z\s]").repeat(1) >> line_break >> match("[A-Za-z\s\.\?]").repeat(1) >> line_break.maybe >> line_break.maybe }
-    rule(:screenplay_element) { scene_heading | action | dialogue }
+    rule(:screenplay_element) { scene_heading | dialogue_block | action }
     rule(:screenplay) { (screenplay_element >> line_break.maybe >> line_break.maybe).repeat(1) }
     root(:screenplay)
 
