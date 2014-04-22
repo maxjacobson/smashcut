6f1b5e8a8da8a6c49a87ba00c57bfd0d8cf66f2e
Max Jacobson <max@hardscrabble.net>
2014-04-21 23:57:13 -0400

    Update parser with more of a bird's eye view

--- lib/smashcut/fountain_parser.rb
+++ lib/smashcut/fountain_parser.rb
@@ -3,9 +3,11 @@ require "parslet"
 class Smashcut
   class FountainParser < Parslet::Parser
     rule(:line_break) { match("\n") }
-    rule(:scene_heading) { match("[A-Z \-\. ]").repeat(1) >> line_break.maybe >> line_break.maybe >> action.maybe }
+    rule(:scene_heading) { match("[A-Z \-\. ]").repeat(1) >> line_break.maybe >> line_break.maybe }
     rule(:action) { match("[a-zA-Z\s\.]").repeat(1) >> line_break.maybe >> line_break.maybe }
-    root(:scene_heading)
+    rule(:screenplay_element) { scene_heading | action }
+    rule(:screenplay) { (screenplay_element >> line_break.maybe >> line_break.maybe).repeat(1) }
+    root(:screenplay)
 
     def parse(text)
       begin
