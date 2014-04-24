aba9e2dda7b8915292f1e0b55059ab94a845dfd1
Max Jacobson <max@hardscrabble.net>
2014-04-23 23:03:42 -0400

    Make all the tests pass!!!
    But in a terrible way.
    
    We've reached the end of the usefulness of the current test suite.
    
    Which is actually awesome, because it means our parser isn't raising
    errors willy-nilly. It has a too-greedy matcher in the action rule. But
    that's OK because that's sort of the design of the Fountain spec, I
    think: anything that isn't identifiable as some specific element is
    probably action.
    
    The next step is to rewrite or extend the tests to assert that dialogue
    is dialogue, not action.

--- lib/smashcut/fountain_parser.rb
+++ lib/smashcut/fountain_parser.rb
@@ -7,7 +7,7 @@ class Smashcut
     rule(:character_name) { match("[A-Z ]").repeat(1) >> line_break }
     rule(:dialogue_speech) { match("[a-zA-Z\s\.\(\)\,0-9\?]").repeat(1) >> line_break.maybe >> line_break.maybe }
     rule(:dialogue_block) { character_name >> line_break >> action >> line_break.maybe >> line_break.maybe }
-    rule(:action) { match("[a-zA-Z\s\.\(\)\,0-9]").repeat(1) >> line_break.maybe >> line_break.maybe }
+    rule(:action) { match("[a-zA-Z\s\.\(\)\,0-9\?\:]").repeat(1) >> line_break.maybe >> line_break.maybe }
     rule(:screenplay_element) { scene_heading | dialogue_block | action }
     rule(:screenplay) { (screenplay_element >> line_break.maybe >> line_break.maybe).repeat(1) }
     root(:screenplay)
