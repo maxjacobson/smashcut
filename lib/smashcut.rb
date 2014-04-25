c0bff7515aeb06c1669e2228259d643aba31a112
Max Jacobson <max@hardscrabble.net>
2014-04-25 02:06:50 +0000

    add some debugging output and guarantee line ending

--- lib/smashcut.rb
+++ lib/smashcut.rb
@@ -5,10 +5,13 @@ class Smashcut
   attr_reader :text
   def initialize(text)
     @text = text
+    @text << "\n" unless @text.end_with?("\n")
   end
 
   def tokens
     @tokens ||= FountainParser.new.parse(text)
+    puts @tokens.inspect
+    @tokens
   end
 
 end
