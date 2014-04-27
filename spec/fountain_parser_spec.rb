78b6caaa92c39d4bb4640e57989347850fb9104f
Max Jacobson <max@hardscrabble.net>
2014-04-26 20:45:25 -0400

    Add a scene number spec and many pending describes
    Basically just copied the http://fountain.io/syntax navigation as
    placeholders into the spec, which is probably going to need to be broken
    into separate files at some point.

--- spec/fountain_parser_spec.rb
+++ spec/fountain_parser_spec.rb
@@ -62,36 +62,96 @@ describe Smashcut::FountainParser do
           end
 
         end
-      end
 
+        describe 'scene numbers' do
+          let(:text) { "EXT. HIKING TRAIL - DAY #1.#" }
+
+          it 'can parse scene headings with scene numbers' do
+            expect(parser).to parse text
+          end
+
+          it 'knows which part is which' do
+            tokens = parser.parse(text)
+            expect(tokens[0][:scene_heading]).to eq "EXT. HIKING - TRAIL - DAY"
+            expect(tokens[0][:scene_number]).to eq "1."
+          end
+
+        end
+
+      end
 
       it 'does not think that trailing dot lines are scene headings' do
         expect(parser.parse('The end.').first[:action]).to eq "The end."
       end
     end
 
-    it 'can parse a scene heading with action' do
-      text = read_fountain 'scene heading with action'
-      expect(parser).to parse text
-      tokens = parser.parse(text)
-      expect(tokens[0][:scene_heading]).to eq "EXT. PARK - DAY"
-      expect(tokens[1][:action]).to eq "A large extended family enjoys a picnic."
+    describe 'action' do
+
+      let(:text) { read_fountain 'scene heading with action' }
+
+      it 'recognizes action following a scene heading' do
+        expect(parser).to parse text
+      end
+
+      it 'knows which part is the scene heading and which part is the action' do
+        tokens = parser.parse(text)
+        expect(tokens[0][:scene_heading]).to eq "EXT. PARK - DAY"
+        expect(tokens[1][:action]).to eq "A large extended family enjoys a picnic."
+      end
+
     end
 
-    it 'can parse a scene with simple dialogue' do
-      text = read_fountain 'scene with simple dialogue'
-      expect(parser).to parse text
+    describe 'character'
+
+    describe 'dialogue' do
+      it 'can parse a scene with simple dialogue' do
+        text = read_fountain 'scene with simple dialogue'
+        expect(parser).to parse text
+      end
     end
 
-    it 'can parse a scene with parenthetical dialogue' do
-      text = read_fountain 'scene with parenthetical dialogue'
-      expect(parser).to parse text
+    describe 'parenthetical' do
+      it 'can parse a scene with parenthetical dialogue' do
+        text = read_fountain 'scene with parenthetical dialogue'
+        expect(parser).to parse text
+      end
     end
 
-    it 'can parse a scene with a transition to another scene' do
-      text = read_fountain 'scene with transition'
-      expect(parser).to parse text
+    describe 'dual dialogue'
+
+    describe 'lyrics'
+
+    describe 'transition' do
+
+      it 'can parse a scene with a transition to another scene' do
+        text = read_fountain 'scene with transition'
+        expect(parser).to parse text
+      end
+
     end
+
+    describe 'centered text'
+
+    describe 'emphasis'
+
+    describe 'title page'
+
+    describe 'page breaks'
+
+    describe 'punctuation'
+
+    describe 'line breaks'
+
+    describe 'indenting'
+
+    describe 'notes'
+
+    describe 'boneyard'
+
+    describe 'sections and synopses'
+
+    describe 'error handling'
+
 
   end
 end
