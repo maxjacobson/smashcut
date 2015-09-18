a402f4623d9c72001799bc60c7e080683028ef69
Max Jacobson <max@hardscrabble.net>
2015-09-18 19:01:20 -0400

    Annotate spec helper, organize spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,24 @@
+# It's nice to have this here, sometimes you want it!
+require "pry"
+
+# The gem!
 require "smashcut"
+
+# Some helpful rspec matchers from parslet
 require "parslet/rig/rspec"
 
-def read_fountain(file)
-  filename = File.expand_path(
-    "./spec/screenplays/#{file.tr(' ', '_')}.fountain")
-  File.read(filename)
-end
+# Our helpers
+Dir.glob("./spec/support/**/*.rb").each { |f| require f }
 
 RSpec.configure do |config|
+  # let's keep it fresh
   config.raise_errors_for_deprecations!
+
+  # tests should pass in any order
+  config.order = :random
+
   config.expect_with :rspec do |c|
+    # TODO: switch over to expect
     c.syntax = :should
   end
 end
---
+++ spec/support/screenplay_file_reading.rb
@@ -0,0 +1,12 @@
+# Some helper mehods related to reading example files from the file system
+module ScreenplayFileReading
+  def read_fountain(file)
+    filename = File.expand_path(
+      "./spec/support/screenplays/#{file.tr(' ', '_')}.fountain")
+    File.read(filename)
+  end
+end
+
+RSpec.configure do |config|
+  config.include ScreenplayFileReading
+end
