9d6a1b55001c73e5ad427a65b2c31340ec2d13da
Max Jacobson <max@hardscrabble.net>
2015-09-21 21:30:32 -0400

    annotate the gnarly regex with some comments

--- lib/smashcut/fountain_emphasis.rb
+++ lib/smashcut/fountain_emphasis.rb
@@ -13,10 +13,19 @@ module Smashcut
       end.flatten.uniq.sort_by(&:length).reverse
     end
 
+    # a regex for identifying whether a line starts out with unemphasized text
+    # and is followed by emphasized text. for emphasis, should match any
+    # combination of *, **, and _
+    #
+    # (if the emphasized text begins with, e.g., _*, it must end with the
+    # reverse: *_
+    #
+    # the emphasized text part is a "zero-width positive look-ahead assertion"
+    # because of the ?= -- so, its presence is required for a string to satisfy
+    # the regex, but when a string is matched against the pattern, only the
+    # unemphasized text is captured
     def stuff_then_emphasis_pattern
-      / #{unemphasized_phrase_regex_str}
-        (?= #{emphasized_phrase_regex_str}) # followed by emphasized stuff
-      /x
+      /#{unemphasized_phrase_regex_str}(?=#{emphasized_phrase_regex_str})/
     end
 
     private
@@ -25,14 +34,21 @@ module Smashcut
       @emphasis_characters ||= ["*", "**", "_"]
     end
 
+    # the question mark here makes this "non-greedy", because we want to match
+    # the first emphasis, not the last one
     def unemphasized_phrase_regex_str
       ".+?"
     end
 
+    # every permutation of emphasis, followed by anything, followed by the
+    # appropriate closing emphasis (the reverse of the opening emphasis)
+    # each one is separated by pipes -- or -- because we don't care which one
+    # is matched
     def emphasized_phrase_regex_str
       longlist.map do |opening_emphasis|
         opening = Regexp.escape(opening_emphasis)
         closing = Regexp.escape(opening_emphasis.reverse)
+        # using ?: here because we don't need to capture this group
         "(?:#{opening}.+#{closing})"
       end.join("|")
     end
