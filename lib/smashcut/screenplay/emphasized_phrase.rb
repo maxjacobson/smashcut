e50382983299a22c1f1ebb5e4f6862c8109ae750
Max Jacobson <max@hardscrabble.net>
2015-09-23 18:31:36 -0400

    add a few methods to EmphasizedPhrase

--- lib/smashcut/screenplay/emphasized_phrase.rb
+++ lib/smashcut/screenplay/emphasized_phrase.rb
@@ -15,6 +15,24 @@ module Smashcut
       def to_fountain
         "#{emphasis}#{text}#{emphasis.reverse}"
       end
+
+      def italicized?
+        [1, 3].include?(star_count)
+      end
+
+      def underlined?
+        emphasis.include?("_")
+      end
+
+      def bolded?
+        [2, 3].include?(star_count)
+      end
+
+      private
+
+      def star_count
+        emphasis.chars.count("*")
+      end
     end
   end
 end
