cb090deee2aa6af663e5fab5b9fc144a1a5c8fed
Max Jacobson <max@hardscrabble.net>
2016-06-15 10:34:01 -0400

    Lock RuboCop (#37)
    * Lock RuboCop
    
    * Exclude guard from travis
    
    It can't install on 2.1.X, and it doesn't need to

--- gems.rb
+++ gems.rb
@@ -5,12 +5,15 @@ gem "bundler", "~> 1.11"
 gem "rake", "~> 11.0"
 gem "rspec", "~> 3.4"
 # TODO(#shipit): bring back rubocop-rspec
-gem "rubocop"
+gem "rubocop", "0.39"
 gem "yard"
-gem "guard"
-gem "guard-rspec"
-gem "guard-rubocop"
 gem "todo_lint"
+
+group :development do
+  gem "guard"
+  gem "guard-rspec"
+  gem "guard-rubocop"
+end
 
 # Specify your gem's dependencies in smashcut.gemspec
 gemspec
