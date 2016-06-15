4372a062d1546b74bb67617dc9ec5111931f3ee9
Max Jacobson <max@hardscrabble.net>
2016-06-15 10:08:37 -0400

    Lock RuboCop

--- gems.rb
+++ gems.rb
@@ -5,7 +5,7 @@ gem "bundler", "~> 1.11"
 gem "rake", "~> 11.0"
 gem "rspec", "~> 3.4"
 # TODO(#shipit): bring back rubocop-rspec
-gem "rubocop"
+gem "rubocop", "0.39"
 gem "yard"
 gem "guard"
 gem "guard-rspec"
