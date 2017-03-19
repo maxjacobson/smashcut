b72e75cedf64a0b25860644cc7dd08a3f92337c6
Max Jacobson <max@hardscrabble.net>
2017-03-19 16:53:26 -0400

    reclassify one todo

--- gems.rb
+++ gems.rb
@@ -5,7 +5,7 @@ gem "bundler", "~> 1.11"
 gem "codeclimate-test-reporter", :group => :test, :require => nil
 gem "rake", "~> 11.0"
 gem "rspec", "~> 3.4"
-# TODO(#shipit): bring back rubocop-rspec
+# TODO(#oneday): bring back rubocop-rspec
 gem "rubocop", "0.46"
 gem "todo_lint"
 gem "yard"
